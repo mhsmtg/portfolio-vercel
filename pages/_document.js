9f4dfb322bbbb442b75782454574e6b55c503768
mhsmtg <82929138+mhsmtg@users.noreply.github.com>
2022-06-19 09:59:12 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'Muhammad Hassan Mughal',
+    description: 'This is the breif portfolio and future blog of Muhammad Hassan Mughal.',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
@@ -17,7 +17,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@TeamKhabib" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
