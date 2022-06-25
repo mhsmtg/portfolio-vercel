59db7bd1fa549c88e451433e6a73a8c862023cfb
mhsmtg <82929138+mhsmtg@users.noreply.github.com>
2022-06-25 22:37:44 +0500

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,25 @@
 const YEAR = new Date().getFullYear()
 
 export default {
+  
+    header: (
+    <small style={{ display: 'block', marginTop: '8rem' }}>
+      <time>{YEAR}</time> © Muhammad Hassan Mughal.
+      <a href="/feed.xml">RSS</a>
+      <style jsx>{`
+        a {
+          float: right;
+        }
+        @media screen and (max-width: 480px) {
+          article {
+            padding-top: 2rem;
+            padding-bottom: 4rem;
+          }
+        }
+      `}</style>
+    </small>
+  )
+  
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Muhammad Hassan Mughal.
